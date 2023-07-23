86bbdb6d7d71be61b581e9ca91fe205734e18c1b
Aleksandr Lambert <95425760+lambert34@users.noreply.github.com>
2023-07-23 22:17:36 +0700

    !

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+$('.design-slider').slick({
+    dots: true,
+    slidesToShow: 4,
+    variableWidth: true
+});
+});
+
+// prevArrow: '<img class="arrow arrow-left" src="images/slider/Arrow2.svg" alt="левая">'
+//     nextArrow: '<img class="arrow arrow-right" src="images/slider/Arrow1.svg" alt="правая">'
